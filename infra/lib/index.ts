54a583668f14150296b55425e209afb1f190b254
Bruno Higino de Souza <brunohigino06@gmail.com>
2023-05-16 20:02:19 -0300

    16.05.2023

--- infra/lib/index.ts
+++ infra/lib/index.ts
@@ -1,4 +1,5 @@
 export { LambdaStack } from './stacks/lambda/lambda-stack';
+export { rdsStack } from './stacks/rds/rds-stack';
 export * from './helpers/integration-helper';
 export * from './helpers/environment-helper';
 export * from './helpers/resource-naming-helper';
